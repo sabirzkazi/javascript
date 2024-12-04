da89396c89ee0b52920bf79c71ccc31ead75dbd3
SabirKazi42 <sabirzkazi42@gmail.com>
2024-12-04 09:10:17 +0530

    commit

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ console.log(cars);
 cars[0] = "opel";
 console.log(cars);
 console.log(cars.length);
+let text = "";
+for (let i = 0; i < cars.length; i++) {
+  text += cars[i] + " ";
+}
+console.log(text);
