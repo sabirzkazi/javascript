5b40ad64756d23dcd50c3fd8675f532c8476df5e
SabirKazi42 <sabirzkazi42@gmail.com>
2024-12-05 14:54:03 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const fruits = ["Apple", "Orange", "Apple", "Mango"];
-console.log(fruits.includes("apple"));
+const numbers = [4, 9, 16, 25, 29];
+
+let first = numbers.find(funOne);
+
+function funOne(value, index, array) {
+  return value > 18;
+}
+
+console.log(first);
